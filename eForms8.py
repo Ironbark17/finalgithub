0366ef559f68c590dd7e9bc09c9ce90a7ae8f532
sameerkha7631@gmail.com <sameerkha7631@gmail.com>
2025-07-11 01:56:11 +0530

    change done

---
+++ eForms8.py
@@ -0,0 +1,18 @@
+
+// Import the LinkedHashMap class
+import java.util.LinkedHashMap;
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedHashMap<String, String> capitalCities = new LinkedHashMap<>();
+
+    capitalCities.put("England", "London");
+    capitalCities.put("India", "New Dehli");
+    capitalCities.put("Austria", "Wien");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("Norway", "Oslo"); // Duplicate
+    capitalCities.put("USA", "Washington DC");
+
+    System.out.println(capitalCities);
+  }
+}
